03e4e433e391d7d90b7306061ac45375ff7836f8
dltldn333 <dltldn333@naver.com>
2026-02-04 20:55:39 +0900

    feat: add test fmd file

--- cmd/writeme/main.go
+++ cmd/writeme/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	target := flag.String("file", "README.fmd", "Target .fmd file to assemble")
+	target := flag.String("file", "WRITEME.fmd", "Target .fmd file to assemble")
 	flag.Parse()
 
 	fmt.Println("WRITEME: Assembling...")
